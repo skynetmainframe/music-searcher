2564b01e6e3d3bf3847b103f6ba9e191fdf4b418
Stefan Kohnen <stefan.kohnen@googlemail.com>
2026-03-17 09:21:46 +0100

    Use a list of SongInfo Elements and display them with @for.

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -9,11 +9,20 @@ import { SongInfo } from '../songinfo';
   styleUrl: './home.css',
 })
 export class Home {
-  song: SongInfo = {
-    id: 0,
-    title: "Never Gonna Give You Up",
-    artist: "Rick Astley",
-    album: "Whenever You Need Somebody",
-    coverPhoto: "/public/album.png",
-  };
+  songList: SongInfo[] = [
+    {
+      id: 0,
+      title: "Never Gonna Give You Up",
+      artist: "Rick Astley",
+      album: "Whenever You Need Somebody",
+      coverPhoto: "/public/album.png",
+    },
+    {
+      id: 1,
+      title: "Eintagsfliege",
+      artist: "Dominik Eulberg",
+      album: "Mannigfaltig",
+      coverPhoto: "/public/album.png",
+    },
+  ];
 }
